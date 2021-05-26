110edc8cb589698af6d8878fc48eff7cd15d2bef
Abimalek <abi.mek2005@gmail.com>
2021-05-26 10:25:27 -0400

    Base Block class was initialized with BlockState functioanlity and TBlock was created as a test block and the rest will be added later in development

--- src/Block.java
+++ src/Block.java
@@ -1,9 +1,43 @@
+import java.awt.*;
 
 public class Block {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int[][][] blockStates;
+	private Color color;
 
+	private int currentState;
+
+	private int xLocation;
+	private int yLocation;
+
+	public Block(Color color, int[][][] blockStates){
+		this.blockStates = blockStates;
+		this.color = color;
+		currentState = 0;
 	}
 
+	public void rotateRight(){
+		this.currentState++;
+		if(this.currentState > 3){
+			this.currentState = 0;
+		}
+	}
+
+	public void rotateLeft()
+	{
+		this.currentState--;
+		if(this.currentState < 0){
+			this.currentState = 3;
+		}
+	}
+	public int getCurrentState(){
+		return currentState;
+	}
+	public int[][] getCurrentStateMap(){
+		return blockStates[getCurrentState()];
+	}
+
+	public boolean canRotate(){
+		return true;
+	}
 }
