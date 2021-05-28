24e8d14bc93d4016b8f8205a35b66f08a87367c3
Samoi-Git <58885969+Samoi-Git@users.noreply.github.com>
2021-05-28 08:48:09 -0400

    popped the fuck off
    wooooooooooooo

--- src/Tetris.java
+++ src/Tetris.java
@@ -19,7 +19,7 @@ public class Tetris extends JPanel {
 	//set the initial width and height of your image
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 1000;
-	private static int size = 50;
+	private static int size = 40;
 
 	//required global variables (initialize these in the constructor) 
 	private BufferedImage image;
@@ -39,7 +39,7 @@ public class Tetris extends JPanel {
 		g = image.getGraphics();
 		g.setColor(Color.blue);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		matrix = new Color[10][20];
+		matrix = new Color[20][10];
 		score = 0;
 		currentBlock = new SquareBlock();
 	
@@ -55,6 +55,18 @@ public class Tetris extends JPanel {
 	}
 	private class Keyboard implements KeyListener {
 		public void keyPressed(KeyEvent e) { 
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				if(canMove(true)) {
+					currentBlock.setxLocation(currentBlock.getxLocation()+1);
+				}
+
+			}
+			if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+				if(canMove(false)) {
+					currentBlock.setxLocation(currentBlock.getxLocation()-1);
+				}
+
+			}
 		  //INCOMPLETE
 		}
 		public void keyReleased(KeyEvent e) { }
@@ -74,17 +86,17 @@ public class Tetris extends JPanel {
 			//vertical lines
 			g.setColor(Color.white);
 			for(int i=1; i<12;i++) {
-				g.drawLine(i*size, size, i*size, 1000);
+				g.drawLine(i*size, size, i*size, size*21);
 			}
 			//horizontial lines
 			g.drawLine(size, size, 200, size);
-			for(int n=size;n<HEIGHT;n=n+size) {
+			for(int n=size;n<size*22;n=n+size) {
 				g.drawLine(size, n, 11*size, n);
 			}
 			for(int i=0;i<matrix.length;i++) {
 				for(int n=0;n<matrix[i].length;n++){
 					if(!(matrix[i][n]==null)){
-						drawBlock(g,(i+1)*size,(n+1)*size,matrix[i][n]);
+						drawBlock(g,(n+1)*size,(i+1)*size,matrix[i][n]);
 
 					}
 				}
@@ -96,6 +108,37 @@ public class Tetris extends JPanel {
 			repaint();
 			currentTime+=deltaTime;
 		}
+		
+	}
+	public static boolean canMove(boolean isRight) {
+		if(isRight){
+			int[][] state = currentBlock.getCurrentStateMap();
+			for(int i=0;i<state.length;i++) {
+				for(int n=0;n<state[i].length;n++){
+					if(state[i][n]==1){
+						if((currentBlock.getxLocation()+n+1)==matrix[0].length||!(matrix[currentBlock.getyLocation()+i][currentBlock.getxLocation()+n+1]==null)) {
+							return false;
+							
+						} 
+					}
+				}
+			}
+			return true;
+		}
+		else {
+			int[][] state = currentBlock.getCurrentStateMap();
+			for(int i=0;i<state.length;i++) {
+				for(int n=0;n<state[i].length;n++){
+					if(state[i][n]==1){
+						if((currentBlock.getxLocation()+n)==0||!(matrix[currentBlock.getyLocation()+i][currentBlock.getxLocation()+n-1]==null)) {
+							return false;
+							
+						} 
+					}
+				}
+			}
+		}
+		return true;
 		
 	}
 
@@ -130,9 +173,11 @@ public class Tetris extends JPanel {
                 if(state[i][n]==1){
 					//if the spot in the matrix directly below the block is occupied, this returns a true for isTouching
 					//if the block is touching the ground, it returns true
-                    if(currentBlock.getyLocation()+i>state.length||(!(matrix[currentBlock.getxLocation()+n][currentBlock.getyLocation()+i+1]==null))) {
+                    if((currentBlock.getyLocation()+i+1)==matrix.length) {
 						return true;
-
+						
+					} else if(!(matrix[currentBlock.getyLocation()+i+1][currentBlock.getxLocation()+n]==null)) {
+						return true;
 					}
                 }
             }
@@ -149,7 +194,8 @@ public class Tetris extends JPanel {
                 if(state[i][n]==1){
 					//if the spot in the matrix directly below the block is occupied, this returns a true for isTouching
 					//if the block is touching the ground, it returns true
-                    matrix[currentBlock.getxLocation()+n][currentBlock.getyLocation()+i]=currentBlock.getColor();
+                    matrix[currentBlock.getyLocation()+i][currentBlock.getxLocation()+n]=currentBlock.getColor();
+
 
 					
                 }
@@ -161,11 +207,11 @@ public class Tetris extends JPanel {
 		//check to see if there's any cleared lines
 		//linescleared is amount of cleared lines
 		int linescleared=0;
-		for(int i=0;i<20;i++) {
+		for(int i=0;i<matrix.length;i++) {
 			//checks to see if the line is complete
 			boolean nonull=true;
-			for(int n=0;n<10;n++) {
-				if(matrix[n][i]==null) {
+			for(int n=0;n<matrix[i].length;n++) {
+				if(matrix[i][n]==null) {
 					nonull=false;
 				}
 			}
@@ -193,19 +239,19 @@ public class Tetris extends JPanel {
 	//call to clear a line and move the ones above it down one
 	public static void moveDown(int line) {
 		//clears the current line
-		for(int n=0;n<10;n++) {
-			matrix[n][line]=null;
+		for(int n=0;n<matrix[line].length;n++) {
+			matrix[line][n]=null;
 		}
 		//moves each line above it down one
 		for(int i=line;i>0;i--) {
-			for(int n=0;n<10;n++){
-				matrix[n][i]=matrix[n][i-1];
+			for(int n=0;n<matrix[i].length;n++){
+				matrix[i][n]=matrix[i-1][n];
 			}
 
 		}
 		//clears the top line
-		for(int n=0;n<10;n++){
-			matrix[n][0]=null;
+		for(int n=0;n<matrix[0].length;n++){
+			matrix[0][n]=null;
 		}
 
 	}
