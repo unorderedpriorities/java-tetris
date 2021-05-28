98059c5ae79d48c9fd4325b2ee6547d776e4d599
Abimalek <abi.mek2005@gmail.com>
2021-05-28 11:02:46 -0400

    AQUA BABBBBY

--- src/LongBlock.java
+++ src/LongBlock.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.awt.*;
 public class LongBlock extends Block {
     public LongBlock(){
-        super(Color.yellow, new int[][][]{
+        super(new Color(0, 255, 255), new int[][][]{
                 {
                         {
                                 0, 0, 0, 0
